96693eebec5ad6b75d599bcc6944e7e5e4eb2aba
CBEngineer <CBEalexanderkevin@gmail.com>
2022-05-27 23:52:38 +0700

    front room, corridor, door

--- js/hotel.js
+++ js/hotel.js
@@ -11,6 +11,92 @@
 
     }
 
+    function loadModels() {
+
+        loader.load(
+            "./room/room.gltf",
+            // model loaded
+            function(gltf){
+    
+                gltf.animations; // Array<THREE.AnimationClip>
+                object = gltf.scene; // THREE.Group
+                gltf.scenes; // Array<THREE.Group>
+                gltf.cameras; // Array<THREE.Camera>
+                gltf.asset; // Object
+    
+                console.log(gltf.scene);
+    
+                // const doorlight = new THREE.PointLight( 0xffff55, 10, 100 );
+                // doorlight.position.set( -5, 5, -10 );
+                // scene.add( doorlight );
+                // const sphereSize = 1;
+                // const pointLightHelper = new THREE.PointLightHelper( doorlight, sphereSize );
+                // scene.add( pointLightHelper );
+
+                // const clips = gltf.animations;
+                // mixer = new THREE.AnimationMixer(gltf.scene)
+                // const action = mixer.clipAction(clips[0])
+                // action.reset().play()
+    
+                scene.add(gltf.scene);
+    
+            },
+    
+            // model loading
+            function(xhr){
+                console.log( ( xhr.loaded / xhr.total * 100 ) + '% loaded' );
+            },
+            // called when loading has errors
+            function ( error ) {
+                console.log( 'An error happened' );
+            }
+        );
+        
+        // loader.load(
+        //     "./doorAlone/door.gltf",
+        //     // model loaded
+        //     function(gltf){
+    
+        //         gltf.animations; // Array<THREE.AnimationClip>
+        //         object = gltf.scene; // THREE.Group
+        //         gltf.scenes; // Array<THREE.Group>
+        //         gltf.cameras; // Array<THREE.Camera>
+        //         gltf.asset; // Object
+    
+        //         console.log(gltf.scene);
+    
+        //         // const doorlight = new THREE.PointLight( 0xffff55, 10, 100 );
+        //         // doorlight.position.set( -5, 5, -10 );
+        //         // scene.add( doorlight );
+        //         // const sphereSize = 1;
+        //         // const pointLightHelper = new THREE.PointLightHelper( doorlight, sphereSize );
+        //         // scene.add( pointLightHelper );
+
+        //         const clips = gltf.animations;
+        //         const mixerDoor = new THREE.AnimationMixer(gltf.scene)
+        //         const action = mixerDoor.clipAction(clips[0])
+        //         action.reset().play()
+    
+        //         scene.add(gltf.scene);
+        //         mixers.push(mixerDoor)
+    
+        //         animate();
+        //     },
+    
+        //     // model loading
+        //     function(xhr){
+        //         console.log( ( xhr.loaded / xhr.total * 100 ) + '% loaded' );
+        //     },
+        //     // called when loading has errors
+        //     function ( error ) {
+        //         console.log( 'An error happened' );
+        //     }
+        // );
+    }
+
+    // on resize
+    window.addEventListener( 'resize', onWindowResize );
+
 
 
     // scene
@@ -18,8 +104,8 @@
 
     // camera
     const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
-    camera.position.set( -5, 2, -6 );
-    camera.lookAt( 0, 0, 0 );
+    camera.position.set( -1, 2, -1 );
+    // camera.lookAt( 0, 0, 0 );
 
     // renderer
     const renderer = new THREE.WebGLRenderer();
@@ -40,11 +126,11 @@
 
     // spotlight 1
     const spotLight = new THREE.SpotLight( 0xffffcc );
-    spotLight.position.set( -4.7, 2.3, -0.7 );
+    spotLight.position.set( -1, 2.0, 1.9 );
     spotLight.castShadow = true;
     scene.add( spotLight );
 
-    spotLight.target.position.set(-4.9, 5, -0.7);
+    spotLight.target.position.set(-1, 5, 1.9);
     scene.add( spotLight.target );
 
     const spotLightHelper = new THREE.SpotLightHelper( spotLight );
@@ -60,37 +146,18 @@
     // loader
     const loader = new THREE.GLTFLoader();
 
+    const mixers = [];
+
+    var mixer;
+
     // Load obj
-    loader.load(
-        "./corridor/scene.gltf",
-        // model loaded
-        function(gltf){
-
-            gltf.animations; // Array<THREE.AnimationClip>
-            object = gltf.scene; // THREE.Group
-            gltf.scenes; // Array<THREE.Group>
-            gltf.cameras; // Array<THREE.Camera>
-            gltf.asset; // Object
-
-            scene.add(gltf.scene);
-
-            animate();
-        },
-
-        // model loading
-        function(xhr){
-            console.log( ( xhr.loaded / xhr.total * 100 ) + '% loaded' );
-        },
-        // called when loading has errors
-        function ( error ) {
-            console.log( 'An error happened' );
-        }
-    );
+    loadModels()
 
     function animate() {
         requestAnimationFrame( animate );
 
-        // mixer.update(clock.getDelta());
+        for ( const mixer of mixers ) mixer.update( delta );
+        spotLightHelper.update();
 
         renderer.render( scene, camera );
     }
